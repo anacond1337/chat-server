7849ffe4bd01dc80441f753d706a2007cf9c73b5
bodzash <bozzaylaszlo4@gmail.com>
2022-11-06 23:10:30 +0100

    feat: created a basic template

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ io.on("connection", (socket) => {
 	socket.on("disconnect", (reason) => {
 		console.log(`User ${socket.id} disconnected. Reason: ${reason}`);
 	});
+
+  socket.on("client_msg", (stream) => {
+    console.log(`${socket.id} sent: ${stream}`);
+    io.emit("server_msg", stream);
+  });
 });
 
 io.listen(8000, { cors: { origin: "*" } });
